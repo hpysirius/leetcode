41f3ba4873da44d265dc093c91c6d7a4efdfbd5e
hui.huang <hui.huang@56qq.com>
2019-08-03 19:33:53 +0800

    feat: 组合总和

---
+++ 39. 组合总和.js
@@ -0,0 +1,21 @@
+var combinationSum = function(candidates, target) {
+    let res = [];
+    let temp = [];
+    helper(res, temp, candidates, target, 0);
+    return res;
+};
+
+const helper = (res, temp, candidates, target, start) => {
+    if(target === 0) {
+        res.push([...temp]);
+        return res;
+    }
+    for(let i = start; i < candidates.length; i++){
+        if(candidates[i] <= target){
+            temp.push(candidates[i]);
+            helper(res, temp, candidates, target - candidates[i], i);
+            temp.length--;
+        }
+    }
+}
+console.log(combinationSum([1, 2, 3, 5, 6, 7], 7));
