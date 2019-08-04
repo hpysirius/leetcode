4330fa1e64dfcd4083b7043cd8fd6f5c8e78d371
hui.huang <hui.huang@56qq.com>
2019-08-04 11:58:44 +0800

    feat: 字符串相加

---
+++ 43. 字符串相乘.js
@@ -0,0 +1,19 @@
+let multiply = function (num1, num2) {
+    let m = num1.length;
+    let n = num2.length;
+    let arr = new Array(m + n).fill(0);
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            let mul = (num1[m] - '0') * (num2[n] - '0');
+            let sum = mul + arr[i + j + 1];
+            arr[i + j] = Math.floor(sum / 10);
+            arr[i + j + 1] = sum % 10;
+        }
+    }
+    let str = arr.reduce((a, b) => {
+        if (a === '' && b === 0) return a;
+        return a + b;
+    }, '');
+
+    return str ? str : '0';
+};
