e8a5680b79d4ce67c5167d1d06d4a77ce46442a0
hui.huang <hui.huang@56qq.com>
2019-08-05 11:39:26 +0800

    feat: 全排列两种

--- 41-60/46. 全排列.js
+++ 41-60/46. 全排列.js
@@ -1,3 +1,4 @@
+// 插入法
 var permute = function (nums) {
     const res = [];
     if (nums.length === 1) {
@@ -13,6 +14,33 @@ var permute = function (nums) {
         return [...new Set(res)];
     }
 };
+// 回溯法（深度优先遍历 + 状态重置）
+var permute = function (nums) {
+    let len = nums.length;
+    const res = [];
+    // nums.sort((a, b) => a - b);
+    const used = new Array(len).fill(false);
+
+    const findPer = (nums, depth, stack) => {
+        if(depth === nums.length){
+            res.push([...stack]);
+            return;
+        }
+        for(let i = 0; i < nums.length; i++){
+            if(!used[i]){
+                // if(i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
+                used[i] = true;
+                stack.push(nums[i]);
+                findPer(nums, depth + 1, stack);
+                stack.pop();
+                used[i] = false;
+            }
+        }
+    }
+    findPer(nums, 0, []);
+    return res;
+};
+
 
 
 console.log(permute([1, 2, 3]));
--- 41-60/46. 全排列II.js
+++ 41-60/46. 全排列II.js
@@ -1,19 +1,29 @@
 var permuteUnique = function (nums) {
+    let len = nums.length;
     const res = [];
-    if (nums.length === 1) {
-        return [nums];
-    } else {
-        let per = permute(nums.slice(1));
-        per.forEach(item => {
-            for (let j = 0; j < item.length + 1; j++) {
-                let temp = item.slice(0, j).concat([nums[0]], item.slice(j));
-                res.push(temp)
+    nums.sort((a, b) => a - b);
+    const used = new Array(len).fill(false);
+
+    const findPermuteUnique = (nums, depth, stack) => {
+        if(depth === nums.length){
+            res.push([...stack]);
+            return;
+        }
+        for(let i = 0; i < nums.length; i++){
+            if(!used[i]){
+                if(i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
+                used[i] = true;
+                stack.push(nums[i]);
+                findPermuteUnique(nums, depth + 1, stack);
+                stack.pop();
+                used[i] = false;
             }
-        });
-        return [...new Set(res)];
+        }
     }
+    findPermuteUnique(nums, 0, []);
+    return res;
 };
 
 
-console.log(permute([1, 2, 3]));
+console.log(permuteUnique([1, 2, 3]));
 
