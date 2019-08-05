985b14670ef166483fcf3d29f8bdedeb8325b586
hui.huang <hui.huang@56qq.com>
2019-08-05 20:16:27 +0800

    feat: 合并区间

--- 41-60/56. 合并区间.js
+++ 41-60/56. 合并区间.js
@@ -1,4 +1,18 @@
-var merge = function(intervals) {
+var merge = function (intervals) {
     const res = [];
-    
+    intervals.sort((a, b) => a[0] - b[0]);
+    let i = 0;
+    while (i < intervals.length) {
+        let left = intervals[i][0];
+        let right = intervals[i][1];
+        while (i < intervals.length - 1 && intervals[i + 1][0] <= right) {
+            i++;
+            right = Math.max(right, intervals[i][1]);
+        }
+        res.push([left, right]);
+        i++;
+    }
+    return res;
 };
+
+console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
