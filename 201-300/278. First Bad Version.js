a4b3af35480adce8d05260cf5be3c59b67e2f709
hui.huang <hui.huang@56qq.com>
2019-08-15 00:13:58 +0800

    feat: 278. First Bad Version

---
+++ 201-300/278. First Bad Version.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let l = 1, r = n;
+        while (l < r) {
+            let mid = l + (r - l >> 1);
+            if (isBadVersion(mid)) r = mid
+            else l = mid + 1;
+        }
+        return l;
+    };
+};
