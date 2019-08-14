1719c2e708bb23b044b6dfd4e5a667976d96cac8
hui.huang <hui.huang@56qq.com>
2019-08-15 00:40:25 +0800

    feat: /**

---
+++ 101-200/162. Find Peak Element.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+    let l = 0, r = nums.length - 1;
+    while (l < r) {
+        let mid = l + r >> 1;
+        if (nums[mid] > nums[mid + 1]) r = mid;
+        else l = mid + 1;
+    }
+    return l;
+};
