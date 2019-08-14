265de432a6f9ee5ab8dd4db94b81ebb775245ca9
hui.huang <hui.huang@56qq.com>
2019-08-14 15:49:03 +0800

    feat: 35.搜索插入位置

--- 1-100/35. 搜索插入位置.js
+++ 1-100/35. 搜索插入位置.js
@@ -3,20 +3,15 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
-    let left = 0;
-    let right = nums.length;
-    while(left < right){
-        let mid = (left + right) >> 1;
-        if(nums[mid] === target) return mid;
-        else if(nums[mid] < target) {
-            left = mid + 1;
-        }else{
-            right = mid
-        }
+const searchInsert = (nums, target) => {
+    let l = 0, r = nums.length - 1;
+    if(nums.length === 0 || nums[r] < target ) return nums.length;
+    while(l < r){
+        let mid = l + r >> 1;
+        if(nums[mid] >= target) r = mid;
+        else l = mid + 1;
     }
-    return left;
-};
-
+    return l;
+}
 
 console.log(searchInsert([1,3,5,6], 2));
