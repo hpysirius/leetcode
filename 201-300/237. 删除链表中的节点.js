49467f02c76414752c4b41c8c049f6495b7b649d
hui.huang <hui.huang@56qq.com>
2019-08-15 23:29:42 +0800

    feat: 237. 删除链表中的节点

---
+++ 201-300/237. 删除链表中的节点.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
+
+// c++ 
+// 一句话
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// class Solution {
+//     public:
+//         void deleteNode(ListNode* node) {
+//             * (node) = * (node -> next);
+//         }
+// };
