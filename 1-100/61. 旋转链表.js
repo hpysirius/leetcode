3b49d607a998e6eff36cd93b41002e114933b443
hui.huang <hui.huang@56qq.com>
2019-08-16 15:33:34 +0800

    feat: 61. 旋转链表

--- 1-100/61. 旋转链表.js
+++ 1-100/61. 旋转链表.js
@@ -1,19 +1,16 @@
 const rotateRight = (head, k) => {
-    if (!head) return null
-    let curr = head, tmp = null, n = 0
-    while (curr) {
-        n++
-        if (!curr.next) {
-            curr.next = head
-            break
-        }
-        curr = curr.next
+    if (!head) return null;
+    let n = 0;
+    for (let p = head; p; p = p.next) n++;
+    k %= n;
+    let first = head, second = head;
+    while(k--) first = first.next;
+    while(first.next){
+        first = first.next;
+        second = second.next;
     }
-    k = k % n
-    while (k++ < n) {
-        k === n && (tmp = head)
-        head = head.next
-    }
-    tmp.next = null
-    return head
+    first.next = head;
+    head = second.next;
+    second.next = null;
+    return head;
 }
