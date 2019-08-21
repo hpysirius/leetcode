b33677f5268186b845b7081ea76e245fd25548f9
hui.huang <hui.huang@56qq.com>
2019-08-21 23:25:56 +0800

    feat: 125. 验证回文串

---
+++ 101-200/125. 验证回文串.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    if (s === '') return true;
+    let reg = /[^A-Za-z0-9]/g// ^表示匹配除了中括号外的字符
+    let str = s.replace(reg, '').toLowerCase();
+    let i = 0, j = str.length - 1;
+    let mid = i + j >> 1;
+    while (i <= mid) {
+        if (str[i++] !== str[j--]) return false;
+    }
+    return true;
+};
+
+console.log(isPalindrome('abb'));
