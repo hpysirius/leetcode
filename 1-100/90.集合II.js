85a1d825d7a1de112625bbb2cde229ce3b59f13e
hui.huang <hui.huang@56qq.com>
2019-08-25 11:27:39 +0800

    feat: 90. 集合II

--- 1-100/90.集合II.js
+++ 1-100/90.集合II.js
@@ -2,25 +2,24 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsetsWithDup = function(nums) {
+var subsetsWithDup = function (nums) {
     let ans = [], path = [];
+    let n = nums.length;
     nums.sort((a, b) => a - b);
     dfs(nums, 0);
     return ans;
-    
+
     function dfs(nums, u) {
-        if(u === nums.length) {
+        if (u === n) {
             ans.push([...path]);
             return;
         }
-        // 计算当前数字的个数
         let k = 0;
-        while(u + k < nums.length && nums[u + k] === nums[u]) k++;
-        for(let i = 0; i <= k; i++){
+        while (u + k < n && nums[u + k] === nums[u]) k++;
+        for (let i = 0; i <= k; i++) {
             dfs(nums, u + k);
             path.push(nums[u]);
         }
-        // 恢复现场
-        for(let i = 0; i <= k; i++) path.pop();
+        for (let i = 0; i <= k; i++) path.pop();
     }
 };
