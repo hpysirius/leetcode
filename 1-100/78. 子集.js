a7af72fdfd37c58672bcbceb4d23517ea6bef9bd
hui.huang <hui.huang@56qq.com>
2019-08-25 11:14:09 +0800

    feat: 集合I

--- 1-100/78. 子集.js
+++ 1-100/78. 子集.js
@@ -1,3 +1,4 @@
+// 位移
 var subsets = function (nums) {
     let res = [];
     for (let i = 0; i < 1 << nums.length; i++) {
@@ -9,3 +10,27 @@ var subsets = function (nums) {
     }
     return res;
 }; 
+
+// 递归
+var subsets = function (nums) {
+    let ans = [], path = [];
+    let n = nums.length;
+    dfs(nums, 0);
+    return ans;
+
+    function dfs(nums, u) {
+        if (u === n) {
+            ans.push([...path]);
+            return;
+        }
+        let k = 0;
+        k++;
+        for (let i = 0; i <= k; i++) {
+            dfs(nums, u + k);
+            path.push(nums[u]);
+        }
+        for (let i = 0; i <= k; i++) path.pop();
+    }
+};
+
+console.log(subsets([1, 2, 3]))
