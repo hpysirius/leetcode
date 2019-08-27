26ddfb020697ede7dcf5cde86b9019d8918ff537
hui.huang <hui.huang@56qq.com>
2019-08-27 14:30:46 +0800

    feat: 合并两个有序数组

--- 1-100/88. 合并两个有序数组.js
+++ 1-100/88. 合并两个有序数组.js
@@ -1,8 +1,8 @@
 const merge = (nums1, m, nums2, n) => {
     let i = m - 1, j = n - 1, k = m + n - 1;
-    while(i >= 0 && j >= 0)
-        if(nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+    while (i >= 0 && j >= 0)
+        if (nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
         else nums1[k--] = nums2[j--];
-    while(j > 0) nums1[k--] = nums2[j--];
+    while (j > 0) nums1[k--] = nums2[j--];
     return nums1;
 }
