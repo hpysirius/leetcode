c8b320d56c34f2f287143e1b0110d36092a56278
hui.huang <hui.huang@56qq.com>
2019-09-01 13:54:18 +0800

    feat: 66.加1

--- 1-100/66. 加一.js
+++ 1-100/66. 加一.js
@@ -2,14 +2,12 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(nums) {
-    let carry = 0, ans  = [];
+var plusOne = function (nums) {
+    let carry = 0, ans = [];
     let l = n = nums.length;
-    while(l-- > 0 || carry > 0) {
+    while (l-- > 0 || carry > 0) {
         let sum = carry;
-        if(l >= 0) {
-            sum += (l === n - 1)  ? nums[l] + 1 : nums[l];
-        }
+        sum += (l === n - 1) ? nums[l] + 1 : (nums[l] || 0);
         ans.unshift(sum % 10);
         carry = Math.floor(sum / 10);
     }
