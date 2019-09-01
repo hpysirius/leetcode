c1f245dc68091c0a579b5595064db66c7b657c45
hui.huang <hui.huang@56qq.com>
2019-09-01 15:35:13 +0800

    fix: 修改443.压缩字符串

--- 401-500/443. 压缩字符串.js
+++ 401-500/443. 压缩字符串.js
@@ -6,7 +6,7 @@ var compress = function (s) {
     let m = s.length, ans = '';
     for (let i = 0; i < m; i++) {
         let k = i;
-        while (s[k] === s[k + 1]) k++;
+        while (k < m && s[k] === s[k + 1]) k++;
         let sum = k - i + 1;
         ans += s[i] + (sum > 1 ? sum : '');
         i = k;
