e57e8b800549b7e0483dae794c67b270b58f9f7f
hui.huang <hui.huang@56qq.com>
2019-09-05 22:23:37 +0800

    fix: 53. 优化最大子序和

--- 201-300/260. 只出现一次的数字 III.js
+++ 201-300/260. 只出现一次的数字 III.js
@@ -4,12 +4,11 @@
  */
 var singleNumber = function (nums) {
     let ans = [], hash = {};
-    for (let i = 0, n = nums.length; i < n; i++) {
+    for (let i = 0, n = nums.length; i < n; i++)
         if (!hash[nums[i]])
             hash[nums[i]] = 1;
         else
             hash[nums[i]]++;
-    }
     for (let key of Object.keys(hash))
         if (hash[key] === 1) ans.push(key);
     return ans;
