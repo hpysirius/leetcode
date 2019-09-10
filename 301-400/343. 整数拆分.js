d3ef60b64f99fa41634ba0695cd21807af339e9f
hui.huang <hui.huang@56qq.com>
2019-09-10 10:29:55 +0800

    feat: 343. 整数拆分

--- 301-400/343. 整数拆分.js
+++ 301-400/343. 整数拆分.js
@@ -6,9 +6,8 @@ var integerBreak = function (n) {
     let f = new Array(n + 1).fill(0);
     f[2] = 1;
     for (let i = 3; i <= n; i++)
-        for (let j = 1; j < i; j++) {
+        for (let j = 1; j < i; j++)
             f[i] = Math.max(f[i], Math.max(j * f[i - j], j * (i - j)))
-        }
     return f[n];
 };
 
