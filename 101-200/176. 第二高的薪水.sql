ba6528d003efb6179d114e9a1cf2422d032b1d4c
hui.huang <hui.huang@56qq.com>
2019-09-23 22:50:48 +0800

    fix: 176. 第二高的薪水

--- 101-200/176. 第二高的薪水.sql
+++ 101-200/176. 第二高的薪水.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select IFNULL (
-        (select distinct Salary from Employee order by Salary desc limit 1 offset 1), 
-        null)
-        as SecondHighestSalary;
+SELECT IFNULL (
+        (SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 offset 1), 
+        NULL)
+        AS SecondHighestSalary;
